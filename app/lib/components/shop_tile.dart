4f80100b2b9c6e712f0c6727da5830989d177087
88JR <101837664+junior-roberto-santos@users.noreply.github.com>
2023-12-05 17:31:09 -0300

    update
    update

--- app/lib/components/shop_tile.dart
+++ app/lib/components/shop_tile.dart
@@ -2,13 +2,27 @@ import 'package:flutter/material.dart';
 
 import '../models/shop.dart';
 
+// ignore: must_be_immutable
 class ShopTile extends StatelessWidget {
-  ShopTile({
-    super.key,
-  });
+  Shop shop;
+
+  ShopTile({super.key, required this.shop});
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return Container(
+      margin: EdgeInsets.only(left: 25),
+      width: 300,
+      decoration: BoxDecoration(
+        color: Colors.grey[1000],
+        
+
+      ),
+      child: Column(children: [
+        Image.asset(shop.imagePath),
+
+      ],
+      ),
+    );
   }
 }
--- app/lib/pages/service_page.dart
+++ app/lib/pages/service_page.dart
@@ -1,9 +1,13 @@
-import 'package:flutter/material.dart';
+import 'dart:ui';
 
+import 'package:flutter/material.dart';
 import '../components/shop_tile.dart';
+import '../models/shop.dart';
 
 class ServicePage extends StatefulWidget {
-  const ServicePage({super.key,});
+  const ServicePage({
+    super.key,
+  });
 
   @override
   State<ServicePage> createState() => _ServicePageState();
@@ -12,52 +16,61 @@ class ServicePage extends StatefulWidget {
 class _ServicePageState extends State<ServicePage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          padding: const EdgeInsets.all(15),
-          margin: const EdgeInsets.symmetric(horizontal: 5),
-          decoration: BoxDecoration(
-              color: Colors.grey[200], borderRadius: BorderRadius.circular(15)),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text('Pesquise'),
-              Icon(
-                Icons.search,
-                color: Colors.black,
-              )
-            ],
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 25.0),
-          child: Text(
-            'Soluções Estética',
-            style: TextStyle(
-                color: Colors.grey[600],
-                fontWeight: FontWeight.w500,
-                fontSize: 30),
-          ),
-        ),
-        Row(
+    return Column(children: [
+      Container(
+        padding: const EdgeInsets.all(15),
+        margin: const EdgeInsets.symmetric(horizontal: 5),
+        decoration: BoxDecoration(
+            color: Colors.grey[200], borderRadius: BorderRadius.circular(15)),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            const Icon(
-              Icons.cut_sharp,
-              size: 30,
+            Text('Pesquise'),
+            Icon(
+              Icons.search,
               color: Colors.black,
-            )
+            ),
           ],
         ),
-        SizedBox(height: 10),
-        Expanded(
-          child: ListView.builder(
-            itemBuilder: (context, index) {
-              return ShopTile();
-            },
-          ),
+      ),
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+        child: Text(
+          'Soluções Estética',
+          style: TextStyle(
+              color: Colors.grey[600],
+              fontWeight: FontWeight.w500,
+              fontSize: 30),
         ),
-      ],
-    );
+      ),
+      Row(
+        children: [
+          const Icon(
+            Icons.cut_sharp,
+            size: 30,
+            color: Colors.black,
+          ),
+        ],
+      ),
+      SizedBox(height: 10),
+      Expanded(
+        child: ListView.builder(
+          itemCount: 5,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            Shop shop = Shop(
+              name: 'Corte',
+              price: '59,99',
+              imagePath: ('lib/IMG_URF/URFHAIR.jpg.jpeg'),
+              description: 'Qualidade e Excelência',
+            );
+
+            return ShopTile(
+              shop: shop,
+            );
+          },
+        ),
+      ),
+    ]);
   }
 }
